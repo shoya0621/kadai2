29ec54ddce3506cbf687955b847cb58e266def4f
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-05 15:17:51 +0000

    編集中1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  delete 'books/:id' => 'books#destroy'
+  post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
