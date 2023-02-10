9244e8a42ea9d6cb2cea598bd5445ee67c499cbd
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-10 18:40:37 +0000

    FINISH

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit',as:'edit_book'
   delete 'books/:id' => 'books#destroy'
   post 'books' => 'books#create'
   patch 'books/:id' => 'books#update'
